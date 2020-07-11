fb664be643a5fba464e408d2fba7bcbbb469c8c7
Jeremy Purser <jeremypurser@gmail.com>
2020-07-11 15:04:27 -0400

    fix: Throw when given invalid arguments

--- src/util/randomNumberFromRange.ts
+++ src/util/randomNumberFromRange.ts
@@ -4,5 +4,10 @@
  * @returns Random integer from min to max
  */
 export const randomNumberFromRange = (min: number, max: number) => {
+  if (min >= max) {
+    throw new Error(
+      'The first argument must be less than the second argument.'
+    );
+  }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
