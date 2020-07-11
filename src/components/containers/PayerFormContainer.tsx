48a0335b40f337ccfdd8c2b2e7a34a9be3f53e1c
Jeremy Purser <jeremypurser@gmail.com>
2020-07-11 11:32:51 -0400

    feat(form): Add phone number to interface

--- src/components/containers/PayerFormContainer.tsx
+++ src/components/containers/PayerFormContainer.tsx
@@ -14,6 +14,7 @@ interface UnmatchedPlan {
   company_state: string;
   company_zip: string;
   company_country: string;
+  company_phone: string;
   effective_date: string;
   display_name: string;
 }
@@ -24,19 +25,14 @@ export interface PayerFormProps {
     id: number;
     name: string;
   }[];
-
   /** select value state */
   selectedMatch: string;
-
   /** button state */
   matchDisabled: boolean;
-
   /** unmatched plan from API */
   unmatchedPlan: UnmatchedPlan | undefined;
-
   /** Change handler */
   handleSelectMatch: (e: React.ChangeEvent<HTMLSelectElement>) => void;
-
   /** match click handler */
   handleClickMatch: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
@@ -49,22 +45,27 @@ export interface PayerFormProps {
 export const payerFormContainer = (
   Screen: React.ComponentType<PayerFormProps>
 ) => () => {
-  const unmatchedPlanId = 2;
+  const unmatchedPlanId = 2; // TODO: make dynamic
 
   const { api } = Environment.current;
 
+  // initial data fetch state
   const [loading, setLoading] = useState(false);
 
+  // select dropdown state
   const [selectedMatch, setSelectedMatch] = useState('');
 
+  // options from API
   const [masterPlans, setMasterPlans] = useState<
     { id: number; name: string }[]
   >([]);
 
+  // current user state
   const [unmatchedPlan, setUnmatchedPlan] = useState<
     UnmatchedPlan | undefined
   >();
 
+  // match button state
   const [matchDisabled, setMatchDisable] = useState(false);
 
   // fetch data needed for screen
@@ -83,6 +84,7 @@ export const payerFormContainer = (
       })
       .finally(() => setLoading(false));
 
+    // Only fetch once when component mounts
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
