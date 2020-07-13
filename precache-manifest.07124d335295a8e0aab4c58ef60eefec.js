97fcd707afaf9486caf7c942af41d253945f5cb1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-13 13:20:38 +0000

    Deploy jeremypurser/roundtrip-react-challenge to github.com/jeremypurser/roundtrip-react-challenge.git:gh-pages

---
+++ precache-manifest.07124d335295a8e0aab4c58ef60eefec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a423ebb53c65f992dae00270514f0ec",
+    "url": "/roundtrip-react-challenge/index.html"
+  },
+  {
+    "revision": "af5e7c4c19163f943bc5",
+    "url": "/roundtrip-react-challenge/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "af5e7c4c19163f943bc5",
+    "url": "/roundtrip-react-challenge/static/js/2.e71ce5cb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/roundtrip-react-challenge/static/js/2.e71ce5cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6361ba711a95731db5",
+    "url": "/roundtrip-react-challenge/static/js/main.e1f748c8.chunk.js"
+  },
+  {
+    "revision": "7881e459b92ec2c5ade1",
+    "url": "/roundtrip-react-challenge/static/js/runtime-main.6205e54b.js"
+  }
+]);
